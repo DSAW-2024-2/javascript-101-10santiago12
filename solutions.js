836efdb48dac9ac94ddf86ccf1a622c89a9f1faf
10santiago12 <surrego1303@gmail.com>
2024-08-14 22:51:42 -0500

    three-solutions

--- solutions.js
+++ solutions.js
@@ -1,29 +1,74 @@
 // Sum of Two Numbers
-function sum(a, b) {
-  // Add your code here
+function sum(a, b) 
+{
+  if (!Number.isFinite(a)||!Number.isFinite(b))
+  {
+  console.log("Los datos de entrada no son adecuados");
+  return;
+  }
+  
+    let suma = a + b;
+  return suma;
 }
 
 // Factorial of a Number
-function factorial(n) {
-  // Add your code here
+function factorial(n) 
+{
+  if (!Number.isFinite(n))
+  {
+  console.log("Los datos de entrada no son adecuados");
+  return;
+  }
+  
+  if(n==0)
+    return 1;
+  else
+  {
+    let fact = 1;
+    for(let i=1; i<=n; i++)
+    {
+      fact *= i;
+    }
+    return fact;
+  }  
 }
 
 // Find the Largest Number
-function findLargest(arr) {
+function findLargest(arr) 
+{
   // Add your code here
 }
 
 // Count Vowels in a String
-function countVowels(str) {
+function countVowels(str) 
+{
   // Add your code here
 }
 
 // Check if a Number is Prime
-function isPrime(n) {
-  // Add your code here
+function isPrime(n) 
+{
+  if (!Number.isFinite(n))
+    {
+    console.log("Los datos de entrada no son adecuados");
+    return;
+    }
+  
+  if (n <= 1) 
+    return false;
+  
+  for (let d = 2; d <= Math.sqrt(n); d++) 
+  {
+    if (n % d === 0) 
+    {
+      return false;
+    }
+  }
+  return true;
 }
 
-module.exports = {
+module.exports = 
+{
   sum,
   factorial,
   findLargest,
