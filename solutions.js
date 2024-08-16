1782a2ae9580fd8e8044c769e07f5800dfd85960
10santiago12 <surrego1303@gmail.com>
2024-08-16 10:16:00 -0500

    Finished

--- solutions.js
+++ solutions.js
@@ -36,24 +36,48 @@ function factorial(n)
 // Find the Largest Number
 function findLargest(arr) 
 {
-  // Add your code here
+  if(Array.isArray(arr)==false)
+  {
+    console.log("Los datos de entrada no son adecuados");
+    return;
+  }
+  /*if(arr.length==0) #En caso de que se quiera validar que el arreglo no este vacio
+  {
+    console.log("El arreglo esta vacio");
+    return;
+  }*/
+  
+    let largest = arr[0];
+  for(let i=1; i<arr.length; i++)
+  {
+    if(arr[i]>largest)
+    {
+      largest = arr[i];
+    }
+  }
+  return largest;
 }
 
 // Count Vowels in a String
 function countVowels(str) 
 {
+  if(typeof str !== 'string')
+  {
+    console.log("Los datos de entrada no son adecuados");
+    return
+  }
+
   let conta=0;
   const vocales = ["a", "e", "i", "o", "u"];
 
   for(let char of str)
   {
-    if(vocales.includes(char))
+    if(vocales.includes(char.toLowerCase()))
     {
       conta++;
     }
   }
   return conta;
-  
 }
 
 // Check if a Number is Prime
